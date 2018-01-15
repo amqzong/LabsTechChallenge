050f5befdb5ca3480396ebca18cbb730093ee4d0
amqzong <AmandaZong@MacBook-Pro.fios-router.home>
2018-01-15 12:48:34 -0500

    Added a page for Amanda Zong.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/amz2136', methods=['GET', 'POST'])
+def index():
+    return render_template('amz2136bio.html')
 
 if __name__ == '__main__':
     app.run()
